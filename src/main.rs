f4775a1b30aef66078b5758beae05c96c4baeabc
Guy Garcia <guy.garcia@securesecrets.com>
2023-10-29 18:37:49 -0400

    cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
 use esp_idf_hal::delay::Ets;
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::prelude::*;
-use esp_idf_hal::spi::{config, Dma, SpiDeviceDriver};
 use esp_idf_hal::spi::config::DriverConfig;
+use esp_idf_hal::spi::{config, Dma, SpiDeviceDriver};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 const FREQUENCY: i64 = 915;
-
-#[allow(dead_code)]
-const IMAGE: [u8; 784] = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 84, 185, 159, 151, 60, 36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 222, 254, 254, 254, 254, 241, 198, 198, 198, 198, 198, 198, 198,
-    198, 170, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 67, 114, 72, 114, 163, 227, 254, 225, 254,
-    254, 254, 250, 229, 254, 254, 140, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 66,
-    14, 67, 67, 67, 59, 21, 236, 254, 106, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 83, 253, 209, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 22, 233, 255, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    129, 254, 238, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 59,
-    249, 254, 62, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 133, 254,
-    187, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 205, 248, 58,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 126, 254, 182, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 75, 251, 240, 57, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 221, 254, 166, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 203, 254, 219, 35, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 254, 254, 77, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 224, 254, 115, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 133, 254, 254, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 61, 242, 254, 254, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 121, 254, 254, 219, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 121, 254, 207, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
@@ -62,55 +31,38 @@ fn main() -> anyhow::Result<()> {
     println!("Initializing SPI");
     let config = config::Config::new().baudrate(8.MHz().into());
     let driver_config = DriverConfig::new().dma(Dma::Disabled);
-    let device =
-        SpiDeviceDriver::new_single(spi, sck, miso, Some(mosi), Some(go), &driver_config, &config)?;
+    let device = SpiDeviceDriver::new_single(
+        spi,
+        sck,
+        miso,
+        Some(mosi),
+        Some(go),
+        &driver_config,
+        &config,
+    )?;
 
     println!("Initializing LoRa");
     let mut lora = sx127x_lora::LoRa::new(device, cs, rts, FREQUENCY, Ets).unwrap();
 
     lora.set_tx_power(17, 1).unwrap();
 
-    let mut init_msg = [0; 255];
-    init_msg[0] = 2;
-    init_msg[1] = 2;
-    init_msg[2] = 2;
+    let send_msg = [11; 255];
 
     #[allow(clippy::empty_loop)]
     loop {
         // Send
         #[cfg(feature = "send")]
         {
-            println!("Sending Image");
+            println!("Sending");
 
-            let mut buffer = [0; 255];
+            if !lora.transmitting().unwrap() {
+                let transmit = lora.transmit_payload(send_msg, 255);
 
-            let mut part: i32 = -1;
-            while part < 3 {
-                if !lora.transmitting().unwrap() {
-                    let mut size = 0;
-                    if part == -1 {
-                        buffer[0] = 2;
-                        buffer[1] = 2;
-                        buffer[2] = 2;
-                        size = 3;
-                    } else {
-                        let limit = if part == 3 { 255 - 19 } else { 255 };
-                        let prefix = (part * 255) as usize;
-                        for b in 0..limit {
-                            buffer[b] = IMAGE[prefix + b];
-                        }
-                        size = limit;
+                match transmit {
+                    Ok(_) => {
+                        println!("Sent packet");
                     }
-
-                    let transmit = lora.transmit_payload(buffer, size);
-
-                    match transmit {
-                        Ok(_) => {
-                            part += 1;
-                            println!("Sent packet");
-                        }
-                        Err(_) => println!("Error"),
-                    }
+                    Err(_) => println!("Error"),
                 }
             }
         }
@@ -118,46 +70,30 @@ fn main() -> anyhow::Result<()> {
         // Receive
         #[cfg(feature = "receive")]
         {
-            let mut packets_received: i32 = -1;
-            let mut received_image = [0; 784];
-            while packets_received < 3 {
-                let poll = lora.poll_irq(Some(2000)); //2 Second timeout
-                match poll {
-                    Ok(_size) => {
-                        let buffer = lora.read_packet().unwrap();
-                        #[cfg(feature = "debug-data")]
-                        {
-                            println!("packet: {:?}", buffer);
+            let poll = lora.poll_irq(Some(2000)); //2 Second timeout
+            match poll {
+                Ok(_size) => {
+                    let buffer = lora.read_packet().unwrap();
+                    println!("packet: {:?}", buffer);
 
-                            let rssi = lora.get_packet_rssi().unwrap();
-                            println!("RSSI: {}", rssi);
+                    let rssi = lora.get_packet_rssi().unwrap();
+                    println!("RSSI: {}", rssi);
 
-                            let snr = lora.get_packet_snr().unwrap();
-                            println!("SNR: {}", snr);
+                    let snr = lora.get_packet_snr().unwrap();
+                    println!("SNR: {}", snr);
 
-                            let err = lora.get_packet_frequency_error().unwrap();
-                            println!("FE: {}", err);
-                        }
+                    let err = lora.get_packet_frequency_error().unwrap();
+                    println!("FE: {}", err);
 
-                        if buffer == init_msg {
-                            println!("Received image request");
-                            packets_received += 1;
-                        } else if packets_received >= 0 {
-                            #[cfg(feature = "debug-data")]
-                            println!("Partial image received");
-                            let limit: usize = if packets_received == 3 { 255 - 19 } else { 255 };
-                            let prefix = (packets_received * 255) as usize;
-                            for b in 0..limit {
-                                received_image[prefix + b] = buffer[b];
-                            }
-
-                            packets_received += 1;
+                    // Check if buffer works
+                    for (i, data) in buffer.iter().enumerate() {
+                        if *data != send_msg[i] {
+                            println!("Mismatch in {}, got {} expected {}", i, send_msg[i], data);
                         }
                     }
-                    Err(_) => println!("Timeout"),
                 }
+                Err(_) => println!("Timeout"),
             }
-            println!("Image: {:?}", received_image);
         }
     }
 }
